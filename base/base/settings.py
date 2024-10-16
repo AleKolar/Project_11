da479c8f4c6d6b5dea6b399276d9530a78822ea9
AleKolar <gefest-173@rambler.ru>
2024-10-16 21:20:11 +0300

    I have created models

--- base/base/settings.py
+++ base/base/settings.py
@@ -75,11 +75,17 @@ WSGI_APPLICATION = 'base.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'app11',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT':5432,
+        'OPTIONS': {
+            'options': '-c client_encoding=UTF8 -c search_path=public'
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
